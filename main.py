796af073016e86fd011e301a99e4b57d76f0f74d
Lucas Hasil <lucashasil@gmail.com>
2021-12-09 13:38:44 +1100

    Implemented BOM weather fetching

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setFixedSize(QSize(800, 500))
+        self.setFixedSize(QSize(1000, 600))
         self.setWindowTitle("My Weather Application")
         
         baseLayout = QGridLayout() # base grid of all locations
@@ -26,10 +26,6 @@ class MainWindow(QMainWindow):
         melLayout = QVBoxLayout()
         perLayout = QVBoxLayout()
 
-        # testwidget = QLabel("test")
-        # testwidget.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        # canLayout.addWidget(testwidget)
 
         canLabel = QLabel("Canberra")
         sydLabel = QLabel("Sydney")
@@ -40,6 +36,57 @@ class MainWindow(QMainWindow):
         melLabel = QLabel("Melbourne")
         perLabel = QLabel("Perth")
 
+
+        canLabelImg = QLabel()
+        sydLabelImg = QLabel()
+        darLabelImg = QLabel()
+        briLabelImg = QLabel()
+        adeLabelImg = QLabel()
+        hobLabelImg = QLabel()
+        melLabelImg = QLabel()
+        perLabelImg = QLabel()
+
+
+        canLabelWeather = QLabel()
+        sydLabelWeather = QLabel()
+        darLabelWeather = QLabel()
+        briLabelWeather = QLabel()
+        adeLabelWeather = QLabel()
+        hobLabelWeather = QLabel()
+        melLabelWeather = QLabel()
+        perLabelWeather = QLabel()
+
+
+        canLabelWeather.setText(self.fetchWeather("can"))
+        sydLabelWeather.setText(self.fetchWeather("syd"))
+        darLabelWeather.setText(self.fetchWeather("dar"))
+        briLabelWeather.setText(self.fetchWeather("bri"))
+        adeLabelWeather.setText(self.fetchWeather("ade"))
+        hobLabelWeather.setText(self.fetchWeather("hob"))
+        melLabelWeather.setText(self.fetchWeather("mel"))
+        perLabelWeather.setText(self.fetchWeather("per"))
+
+
+        canLabelImg.setScaledContents(True)
+        sydLabelImg.setScaledContents(True)
+        darLabelImg.setScaledContents(True)
+        briLabelImg.setScaledContents(True)
+        adeLabelImg.setScaledContents(True)
+        hobLabelImg.setScaledContents(True)
+        melLabelImg.setScaledContents(True)
+        perLabelImg.setScaledContents(True)
+
+    
+        canLabelImg.setPixmap(QPixmap('test.jpg'))
+        sydLabelImg.setPixmap(QPixmap('test.jpg'))
+        darLabelImg.setPixmap(QPixmap('test.jpg'))
+        briLabelImg.setPixmap(QPixmap('test.jpg'))
+        adeLabelImg.setPixmap(QPixmap('test.jpg'))
+        hobLabelImg.setPixmap(QPixmap('test.jpg'))
+        melLabelImg.setPixmap(QPixmap('test.jpg'))
+        perLabelImg.setPixmap(QPixmap('test.jpg'))
+
+
         canLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
         sydLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
         darLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
@@ -49,6 +96,14 @@ class MainWindow(QMainWindow):
         melLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
         perLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
 
+        canLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        sydLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        darLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        briLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        adeLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        hobLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        melLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        perLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         canLayout.addWidget(canLabel)
         sydLayout.addWidget(sydLabel)
@@ -58,6 +113,27 @@ class MainWindow(QMainWindow):
         hobLayout.addWidget(hobLabel)
         melLayout.addWidget(melLabel)
         perLayout.addWidget(perLabel)
+
+
+        canLayout.addWidget(canLabelImg)
+        sydLayout.addWidget(sydLabelImg)
+        darLayout.addWidget(darLabelImg)
+        briLayout.addWidget(briLabelImg)
+        adeLayout.addWidget(adeLabelImg)
+        hobLayout.addWidget(hobLabelImg)
+        melLayout.addWidget(melLabelImg)
+        perLayout.addWidget(perLabelImg)
+
+
+        canLayout.addWidget(canLabelWeather)
+        sydLayout.addWidget(sydLabelWeather)
+        darLayout.addWidget(darLabelWeather)
+        briLayout.addWidget(briLabelWeather)
+        adeLayout.addWidget(adeLabelWeather)
+        hobLayout.addWidget(hobLabelWeather)
+        melLayout.addWidget(melLabelWeather)
+        perLayout.addWidget(perLabelWeather)
+
 
         baseLayout.addLayout(canLayout, 0, 0)
         baseLayout.addLayout(sydLayout, 0, 1)
@@ -74,67 +150,38 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
 
+    def fetchWeather(self, location):
+        match location:
+            case "can":
+                url = "http://www.bom.gov.au/fwo/IDN60903/IDN60903.94926.json"
+            case "syd":
+                url = "http://www.bom.gov.au/fwo/IDN60901/IDN60901.95766.json"
+            case "dar":
+                url = "http://www.bom.gov.au/fwo/IDD60901/IDD60901.95122.json"
+            case "bri":
+                url = "http://www.bom.gov.au/fwo/IDQ60901/IDQ60901.94576.json"
+            case "ade":
+                url = "http://www.bom.gov.au/fwo/IDS60901/IDS60901.94672.json"
+            case "hob":
+                url = "http://www.bom.gov.au/fwo/IDT60901/IDT60901.94970.json"
+            case "mel":
+                url = "http://www.bom.gov.au/fwo/IDV60901/IDV60901.95936.json"
+            case "per":
+                url = "http://www.bom.gov.au/fwo/IDW60901/IDW60901.94608.json"
 
-    # def main():
-        # window = tk.Tk()
-        # window.geometry("800x500")
-        # window.title("The Weather")
-        # window.grid_rowconfigure(0, weight=1)
-        # window.grid_columnconfigure(0, weight=1)
-        # window.resizable(0, 0)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'} # needed to authenticate, need user-agnt
+        request = requests.get(url, headers=headers)
+        jsonContent = json.loads(request.content)
+        # print(jsonContent['observations']['data'][0]['name']) # this is index for station location
+        # print(jsonContent['observations']['data'][0]["local_date_time"])
+        # print(jsonContent['observations']['data'][0]["air_temp"]) # 0 is the most recent weather report
+        # if (jsonContent['observations']['data'][0]["cloud"] == "-"):
+        #     print("Clear")
+        # else:
+        #     print(jsonContent['observations']['data'][0]["cloud"])
+        # print(str(jsonContent['observations']['data'][0]["rel_hum"]) + '%')
+        return (jsonContent['observations']['data'][0]['name'] + " " + jsonContent['observations']['data'][0]["local_date_time"] + " " + str(jsonContent['observations']['data'][0]["air_temp"]) + " " + str(jsonContent['observations']['data'][0]["rel_hum"]) + "%")
 
-        # canLayout = QVBoxLayout()
-        # sydLayout = QVBoxLayout()
-        # darLayout = QVBoxLayout()
-        # briLayout = QVBoxLayout()
-        # adeLayout = QVBoxLayout()
-        # hobLayout = QVBoxLayout()
-        # melLayout = QVBoxLayout()
-        # perLayout = QVBoxLayout()
-
-        # canLab = tk.Label(canLayout, text="Canberra").grid(row=0, column=0, padx=10, pady=10)
-        # sydLab = tk.Label(sydLayout, text="Sydney").grid(row=0, column=1, padx=10, pady=10)
-        # darLab = tk.Label(darLayout, text="Darwin").grid(row=0, column=2, padx=10, pady=10)
-        # briLab = tk.Label(briLayout, text="Brisbane").grid(row=1, column=0, padx=10, pady=10)
-        # adeLab = tk.Label(adeLayout, text="Adelaide").grid(row=1, column=1, padx=10, pady=10)
-        # hobLab = tk.Label(hobLayout, text="Hobart").grid(row=1, column=2, padx=10, pady=10)
-        # melLab = tk.Label(melLayout, text="Melbourne").grid(row=2, column=0, padx=10, pady=10)
-        # perLab = tk.Label(perLayout, text="Perth").grid(row=2, column=1, padx=10, pady=10)
-        # window.mainloop()
-
-
-
-
-    # def fetchWeather(location):
-    #     match location:
-    #         case "can":
-    #             url = "http://www.bom.gov.au/fwo/IDN60903/IDN60903.94926.json"
-    #         case "syd":
-    #             url = "http://www.bom.gov.au/fwo/IDN60901/IDN60901.95766.json"
-    #         case "dar":
-    #             url = "http://www.bom.gov.au/fwo/IDD60901/IDD60901.95122.json"
-    #         case "bri":
-    #             url = "http://www.bom.gov.au/fwo/IDQ60901/IDQ60901.94576.json"
-    #         case "ade":
-    #             url = "http://www.bom.gov.au/fwo/IDS60901/IDS60901.94672.json"
-    #         case "hob":
-    #             url = "http://www.bom.gov.au/fwo/IDT60901/IDT60901.94970.json"
-    #         case "mel":
-    #             url = "http://www.bom.gov.au/fwo/IDV60901/IDV60901.95936.json"
-    #         case "per":
-    #             url = "http://www.bom.gov.au/fwo/IDW60901/IDW60901.94608.json"
-
-    #     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'} # needed to authenticate, need user-agnt
-    #     request = requests.get(url, headers=headers)
-    #     jsonContent = json.loads(request.content)
-    #     print(jsonContent['observations']['data'][0]['name']) # this is index for station location
-    #     print(jsonContent['observations']['data'][0]["local_date_time"])
-    #     print(jsonContent['observations']['data'][0]["air_temp"]) # 0 is the most recent weather report
-    #     if (jsonContent['observations']['data'][0]["cloud"] == "-"):
-    #         print("Clear")
-    #     else:
-    #         print(jsonContent['observations']['data'][0]["cloud"])
-    #     print(str(jsonContent['observations']['data'][0]["rel_hum"]) + '%')
 
 app = QApplication([])
 window = MainWindow()
