37908f4d0a89b9efba47ca2241620b7dbdcf61d3
Lucas Hasil <lucashasil@gmail.com>
2021-12-09 18:37:29 +1100

    Basic updating method added

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
+
 import sys
 import json
 import requests
-#from requests.api import request
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,160 +12,167 @@ class MainWindow(QMainWindow):
 
         self.setFixedSize(QSize(1000, 800))
         self.setWindowTitle("My Weather Application")
-        
-        baseLayout = QGridLayout() # base grid of all locations
+
+
+        self.baseLayout = QGridLayout() # base grid of all locations
 
         # create separate vertical box layout for each location which will be 
         # put in the grid individually
-        canLayout = QVBoxLayout()
-        sydLayout = QVBoxLayout()
-        darLayout = QVBoxLayout()
-        briLayout = QVBoxLayout()
-        adeLayout = QVBoxLayout()
-        hobLayout = QVBoxLayout()
-        melLayout = QVBoxLayout()
-        perLayout = QVBoxLayout()
+        self.canLayout = QVBoxLayout()
+        self.sydLayout = QVBoxLayout()
+        self.darLayout = QVBoxLayout()
+        self.briLayout = QVBoxLayout()
+        self.adeLayout = QVBoxLayout()
+        self.hobLayout = QVBoxLayout()
+        self.melLayout = QVBoxLayout()
+        self.perLayout = QVBoxLayout()
 
 
-        canLabel = QLabel("Canberra")
-        sydLabel = QLabel("Sydney")
-        darLabel = QLabel("Darwin")
-        briLabel = QLabel("Brisbane")
-        adeLabel = QLabel("Adelaide")
-        hobLabel = QLabel("Hobart")
-        melLabel = QLabel("Melbourne")
-        perLabel = QLabel("Perth")
+        self.canLabel = QLabel("Canberra")
+        self.sydLabel = QLabel("Sydney")
+        self.darLabel = QLabel("Darwin")
+        self.briLabel = QLabel("Brisbane")
+        self.adeLabel = QLabel("Adelaide")
+        self.hobLabel = QLabel("Hobart")
+        self.melLabel = QLabel("Melbourne")
+        self.perLabel = QLabel("Perth")
 
 
-        canLabelImg = QLabel()
-        sydLabelImg = QLabel()
-        darLabelImg = QLabel()
-        briLabelImg = QLabel()
-        adeLabelImg = QLabel()
-        hobLabelImg = QLabel()
-        melLabelImg = QLabel()
-        perLabelImg = QLabel()
+        self.canLabelImg = QLabel()
+        self.sydLabelImg = QLabel()
+        self.darLabelImg = QLabel()
+        self.briLabelImg = QLabel()
+        self.adeLabelImg = QLabel()
+        self.hobLabelImg = QLabel()
+        self.melLabelImg = QLabel()
+        self.perLabelImg = QLabel()
 
 
-        canLabelWeather = QLabel()
-        sydLabelWeather = QLabel()
-        darLabelWeather = QLabel()
-        briLabelWeather = QLabel()
-        adeLabelWeather = QLabel()
-        hobLabelWeather = QLabel()
-        melLabelWeather = QLabel()
-        perLabelWeather = QLabel()
+        self.canLabelWeather = QLabel()
+        self.sydLabelWeather = QLabel()
+        self.darLabelWeather = QLabel()
+        self.briLabelWeather = QLabel()
+        self.adeLabelWeather = QLabel()
+        self.hobLabelWeather = QLabel()
+        self.melLabelWeather = QLabel()
+        self.perLabelWeather = QLabel()
 
 
-        canLabelWeather.setText(self.fetchWeather("can"))
-        sydLabelWeather.setText(self.fetchWeather("syd"))
-        darLabelWeather.setText(self.fetchWeather("dar"))
-        briLabelWeather.setText(self.fetchWeather("bri"))
-        adeLabelWeather.setText(self.fetchWeather("ade"))
-        hobLabelWeather.setText(self.fetchWeather("hob"))
-        melLabelWeather.setText(self.fetchWeather("mel"))
-        perLabelWeather.setText(self.fetchWeather("per"))
+        self.canLabelWeather.setText(self.fetchWeather("can"))
+        self.sydLabelWeather.setText(self.fetchWeather("syd"))
+        self.darLabelWeather.setText(self.fetchWeather("dar"))
+        self.briLabelWeather.setText(self.fetchWeather("bri"))
+        self.adeLabelWeather.setText(self.fetchWeather("ade"))
+        self.hobLabelWeather.setText(self.fetchWeather("hob"))
+        self.melLabelWeather.setText(self.fetchWeather("mel"))
+        self.perLabelWeather.setText(self.fetchWeather("per"))
 
 
-        canLabelImg.setScaledContents(True)
-        sydLabelImg.setScaledContents(True)
-        darLabelImg.setScaledContents(True)
-        briLabelImg.setScaledContents(True)
-        adeLabelImg.setScaledContents(True)
-        hobLabelImg.setScaledContents(True)
-        melLabelImg.setScaledContents(True)
-        perLabelImg.setScaledContents(True)
+        self.canLabelImg.setScaledContents(True)
+        self.sydLabelImg.setScaledContents(True)
+        self.darLabelImg.setScaledContents(True)
+        self.briLabelImg.setScaledContents(True)
+        self.adeLabelImg.setScaledContents(True)
+        self.hobLabelImg.setScaledContents(True)
+        self.melLabelImg.setScaledContents(True)
+        self.perLabelImg.setScaledContents(True)
 
     
-        canLabelImg.setPixmap(QPixmap('test.jpg'))
-        sydLabelImg.setPixmap(QPixmap('test.jpg'))
-        darLabelImg.setPixmap(QPixmap('test.jpg'))
-        briLabelImg.setPixmap(QPixmap('test.jpg'))
-        adeLabelImg.setPixmap(QPixmap('test.jpg'))
-        hobLabelImg.setPixmap(QPixmap('test.jpg'))
-        melLabelImg.setPixmap(QPixmap('test.jpg'))
-        perLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.canLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.sydLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.darLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.briLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.adeLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.hobLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.melLabelImg.setPixmap(QPixmap('test.jpg'))
+        self.perLabelImg.setPixmap(QPixmap('test.jpg'))
 
 
-        canLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
-        sydLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
-        darLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
-        briLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        adeLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
-        hobLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
-        melLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
-        perLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        self.canLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        self.sydLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        self.darLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        self.briLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.adeLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        self.hobLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        self.melLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
+        self.perLabel.setAlignment(Qt.AlignmentFlag.AlignCenter) 
 
-        canLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        sydLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        darLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        briLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        adeLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        hobLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        melLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        perLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.canLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.sydLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.darLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.briLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.adeLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.hobLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.melLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.perLabelWeather.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        canLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
-        sydLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
-        darLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
-        briLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
-        adeLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
-        hobLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
-        melLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
-        perLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.canLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.sydLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.darLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.briLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.adeLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.hobLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.melLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
+        self.perLabel.setStyleSheet("font-weight: bold; text-decoration: underline;")
 
-        canLabelWeather.setStyleSheet("border: 1.3px solid black;")
-        sydLabelWeather.setStyleSheet("border: 1.3px solid black;")
-        darLabelWeather.setStyleSheet("border: 1.3px solid black;")
-        briLabelWeather.setStyleSheet("border: 1.3px solid black;")
-        adeLabelWeather.setStyleSheet("border: 1.3px solid black;")
-        hobLabelWeather.setStyleSheet("border: 1.3px solid black;")
-        melLabelWeather.setStyleSheet("border: 1.3px solid black;")
-        perLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.canLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.sydLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.darLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.briLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.adeLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.hobLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.melLabelWeather.setStyleSheet("border: 1.3px solid black;")
+        self.perLabelWeather.setStyleSheet("border: 1.3px solid black;")
 
-        canLayout.addWidget(canLabel)
-        sydLayout.addWidget(sydLabel)
-        darLayout.addWidget(darLabel)
-        briLayout.addWidget(briLabel)
-        adeLayout.addWidget(adeLabel)
-        hobLayout.addWidget(hobLabel)
-        melLayout.addWidget(melLabel)
-        perLayout.addWidget(perLabel)
-
-
-        canLayout.addWidget(canLabelImg)
-        sydLayout.addWidget(sydLabelImg)
-        darLayout.addWidget(darLabelImg)
-        briLayout.addWidget(briLabelImg)
-        adeLayout.addWidget(adeLabelImg)
-        hobLayout.addWidget(hobLabelImg)
-        melLayout.addWidget(melLabelImg)
-        perLayout.addWidget(perLabelImg)
+        self.canLayout.addWidget(self.canLabel)
+        self.sydLayout.addWidget(self.sydLabel)
+        self.darLayout.addWidget(self.darLabel)
+        self.briLayout.addWidget(self.briLabel)
+        self.adeLayout.addWidget(self.adeLabel)
+        self.hobLayout.addWidget(self.hobLabel)
+        self.melLayout.addWidget(self.melLabel)
+        self.perLayout.addWidget(self.perLabel)
 
 
-        canLayout.addWidget(canLabelWeather)
-        sydLayout.addWidget(sydLabelWeather)
-        darLayout.addWidget(darLabelWeather)
-        briLayout.addWidget(briLabelWeather)
-        adeLayout.addWidget(adeLabelWeather)
-        hobLayout.addWidget(hobLabelWeather)
-        melLayout.addWidget(melLabelWeather)
-        perLayout.addWidget(perLabelWeather)
+        self.canLayout.addWidget(self.canLabelImg)
+        self.sydLayout.addWidget(self.sydLabelImg)
+        self.darLayout.addWidget(self.darLabelImg)
+        self.briLayout.addWidget(self.briLabelImg)
+        self.adeLayout.addWidget(self.adeLabelImg)
+        self.hobLayout.addWidget(self.hobLabelImg)
+        self.melLayout.addWidget(self.melLabelImg)
+        self.perLayout.addWidget(self.perLabelImg)
 
 
-        baseLayout.addLayout(canLayout, 0, 0)
-        baseLayout.addLayout(sydLayout, 0, 1)
-        baseLayout.addLayout(darLayout, 0, 2)
-        baseLayout.addLayout(briLayout, 1, 0)
-        baseLayout.addLayout(adeLayout, 1, 1)
-        baseLayout.addLayout(hobLayout, 1, 2)
-        baseLayout.addLayout(melLayout, 2, 0)
-        baseLayout.addLayout(perLayout, 2, 2)
+        self.canLayout.addWidget(self.canLabelWeather)
+        self.sydLayout.addWidget(self.sydLabelWeather)
+        self.darLayout.addWidget(self.darLabelWeather)
+        self.briLayout.addWidget(self.briLabelWeather)
+        self.adeLayout.addWidget(self.adeLabelWeather)
+        self.hobLayout.addWidget(self.hobLabelWeather)
+        self.melLayout.addWidget(self.melLabelWeather)
+        self.perLayout.addWidget(self.perLabelWeather)
 
 
-        widget = QWidget() # base widget
-        widget.setLayout(baseLayout)
-        self.setCentralWidget(widget)
+        self.baseLayout.addLayout(self.canLayout, 0, 0)
+        self.baseLayout.addLayout(self.sydLayout, 0, 1)
+        self.baseLayout.addLayout(self.darLayout, 0, 2)
+        self.baseLayout.addLayout(self.briLayout, 1, 0)
+        self.baseLayout.addLayout(self.adeLayout, 1, 1)
+        self.baseLayout.addLayout(self.hobLayout, 1, 2)
+        self.baseLayout.addLayout(self.melLayout, 2, 0)
+        self.baseLayout.addLayout(self.perLayout, 2, 2)
+
+
+        self.widget = QWidget() # base widget
+        self.widget.setLayout(self.baseLayout)
+        self.setCentralWidget(self.widget)
+
+        self.timer = QTimer(self)
+        self.timer.setInterval(300000) # 5 minute intervals
+        self.timer.setSingleShot(False)
+        self.timer.timeout.connect(self.updateWeather)
+        self.timer.start(300000) # 5 minute intervals
 
 
     def fetchWeather(self, location):
@@ -205,9 +212,19 @@ class MainWindow(QMainWindow):
         + "Temperature: " + temp + u"\N{DEGREE SIGN}" + "C" + "\n"
         + "Humidity: " + hum)
 
+    def updateWeather(self):
+        self.canLabelWeather.setText(self.fetchWeather("can"))
+        self.sydLabelWeather.setText(self.fetchWeather("syd"))
+        self.darLabelWeather.setText(self.fetchWeather("dar"))
+        self.briLabelWeather.setText(self.fetchWeather("bri"))
+        self.adeLabelWeather.setText(self.fetchWeather("ade"))
+        self.hobLabelWeather.setText(self.fetchWeather("hob"))
+        self.melLabelWeather.setText(self.fetchWeather("mel"))
+        self.perLabelWeather.setText(self.fetchWeather("per"))
+        print("updated")
+
 
 app = QApplication([])
 window = MainWindow()
 window.show()
-
 app.exec()
