c96cef93db33688f0be826351818c98750ac6f22
Lucas Hasil <lucashasil@gmail.com>
2022-01-12 17:08:15 +1100

    Added more appropriate comments

--- basic_weather.py
+++ basic_weather.py
@@ -8,24 +8,29 @@ import time
 
 class MainWindow(QMainWindow):
 
+    # Store a list of each capital city of Australia
     cities = ["Adelaide", "Brisbane", "Canberra", "Darwin", "Hobart"
              , "Melbourne", "Perth", "Sydney"]
 
     def __init__(self):
         super().__init__()
 
+        # Initialise the window for this mode
         self.setFixedSize(QSize(1000, 700))
         self.setWindowTitle("SimpleWeatherApplication - Basic")
 
+        # Use a grid based layout for this mode
+        self.baseLayout = QGridLayout()
 
-        self.baseLayout = QGridLayout() # base grid of all locations
-
+        # Iterate over every capital city and create new QWidgets for each 
+        # appropriate item for EACH capital city, appending each QWidget to
+        # a sublist of this 2D list.
         self.citiesList = []
 
         for i in range(len(self.cities)):
             self.citiesList.append([])
 
-        # add the layouts and their respective widgets to their own lists
+        # Add the layouts and their respective widgets to their own lists
         for i in range(len(self.cities)):
             self.citiesList[i].append(QVBoxLayout())
             self.citiesList[i].append(QLabel(self.cities[i]))
@@ -33,7 +38,7 @@ class MainWindow(QMainWindow):
             self.citiesList[i][2].setPixmap(QPixmap("images/" + self.cities[i] + ".jpg"))
             self.citiesList[i].append(QLabel(self.fetchWeather(self.cities[i])))
 
-        # add the widgets to their layouts
+        # Add the widgets to their correct layouts
         for i in range(len(self.citiesList)):
             for j in range(1, len(self.citiesList[i])):
                 self.citiesList[i][2].setScaledContents(True)
@@ -43,13 +48,15 @@ class MainWindow(QMainWindow):
                 self.citiesList[i][3].setStyleSheet("border: 1.3px solid black;")
                 self.citiesList[i][0].addWidget(self.citiesList[i][j])
 
-        # fill final grid slot with basic information
+        # Create a final layout which acts as a basic information widget which
+        # will take up the final grid spot. This will simply display the last
+        # time the application had attempted to update the weather information.
         self.infoLayout = QVBoxLayout()
-        self.lastUpdate = QLabel(self.updateTime())
+        self.lastUpdate = QLabel(self.updateTime()) # Use the correct method
         self.lastUpdate.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.infoLayout.addWidget(self.lastUpdate)
 
-
+        # Add each city and it's layout to the base layout of the window itself
         self.baseLayout.addLayout(self.citiesList[0][0], 0, 0)
         self.baseLayout.addLayout(self.citiesList[1][0], 0, 1)
         self.baseLayout.addLayout(self.citiesList[2][0], 0, 2)
@@ -60,19 +67,25 @@ class MainWindow(QMainWindow):
         self.baseLayout.addLayout(self.infoLayout, 2, 1)
         self.baseLayout.addLayout(self.citiesList[7][0], 2, 2)
 
-
+        # Use a dummy widget and set it's layout to the main layout
         self.widget = QWidget() # base widget
         self.widget.setLayout(self.baseLayout)
         self.setCentralWidget(self.widget)
 
+        # Setup a basic timer (a QTimer) and set it to call the updateWeather
+        # method every 5 minutes.
         self.timer = QTimer(self)
-        self.timer.setInterval(300000) # 5 minute intervals
+        self.timer.setInterval(300000)
         self.timer.setSingleShot(False)
         self.timer.timeout.connect(self.updateWeather)
-        self.timer.start(300000) # 5 minute intervals
+        self.timer.start(300000)
 
-
+    # The basic method for fetching weather from the BOM database
     def fetchWeather(self, location):
+        # Note this requires Python 3.10+ due to the match statement. Each of 
+        # the following JSON files can be found easily via a quick search on
+        # the BOM website. If these change in the future this application will
+        # break.
         match location:
             case "Canberra":
                 url = "http://www.bom.gov.au/fwo/IDN60903/IDN60903.94926.json"
@@ -91,9 +104,20 @@ class MainWindow(QMainWindow):
             case "Perth":
                 url = "http://www.bom.gov.au/fwo/IDW60901/IDW60901.94608.json"
 
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'} # needed to authenticate, need user-agnt
+        # We need to use the appropriate headers when fetching content such that
+        # the url will not reject the request. The following headers correspond
+        # to a Firefox browser.
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'}
+
+        # Use requests with the correct headers to fetch the content string
         request = requests.get(url, headers=headers)
+
+        # Convert this string to a JSON object using the json library
         jsonContent = json.loads(request.content)
+
+        # Access the relevant weather data by simply accessing the json object
+        # directly. See the above links for a specific breakdown of the files
+        # indices.
         statLoc = jsonContent['observations']['data'][0]['name']
         locTime = jsonContent['observations']['data'][0]["local_date_time"]
         temp = str(jsonContent['observations']['data'][0]["air_temp"])
@@ -102,17 +126,21 @@ class MainWindow(QMainWindow):
             hum = "0%"
         else:
             hum = humPer + "%"
-
+        # Return all of this information as a single string.
         return ("Station Location: " + statLoc + "\n"
         + "Last Update (Local Time): " + locTime[3:len(locTime)] + "\n"
         + "Temperature: " + temp + u"\N{DEGREE SIGN}" + "C" + "\n"
         + "Humidity: " + hum)
 
+    # Every 5 minutes we call the following method which simply sets the weather
+    # for each city again, acting as an update.
     def updateWeather(self):
         for i in range(len(self.citiesList)):
             self.citiesList[i][3].setText(self.fetchWeather(self.cities[i]))
         self.lastUpdate.setText(self.updateTime())
-
+   
+    # We also need to update the time which we last updated the weatherm, for
+    # this we simply use the time library.
     def updateTime(self):
         t = time.localtime()
         curTime = time.strftime("%I:%M%p", t).lower()
--- main.py
+++ main.py
@@ -10,18 +10,20 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        # Setup initial window
         self.setFixedSize(QSize(800, 500))
         self.setWindowTitle("SimpleWeatherApplication")
         self.setStyleSheet("MainWindow {border-image: url(images/gradient.jpg)}")
 
-
+        # Setup main layouts for the window itself and appropriate QWidgets
         self.mainLayout = QVBoxLayout()
         self.subLayout = QHBoxLayout()
 
         self.choiceLabel = QLabel("Please select which mode you would like to run:")
         self.choiceLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.mainLayout.addWidget(self.choiceLabel)
-
+        
+        # Add a button for each basic and extended modes
         self.basicMode = QPushButton("Basic Mode")
         self.basicMode.setFixedHeight(125)
         self.subLayout.addWidget(self.basicMode)
@@ -30,41 +32,42 @@ class MainWindow(QMainWindow):
         self.extendedMode.setFixedHeight(125)
         self.subLayout.addWidget(self.extendedMode)
 
-
         self.mainLayout.addLayout(self.subLayout)
 
         self.emptyLabel = QLabel()
         self.emptyLabel.setFixedHeight(80)
         self.mainLayout.addWidget(self.emptyLabel)
 
-
+        # Create a dummy widget and set it's layout manager to the main layout
         self.widget = QWidget()
         self.widget.setLayout(self.mainLayout)
         self.setCentralWidget(self.widget)
 
-        # connect buttons to appropriate methods
-        self.basicMode.clicked.connect(self.basicWindow)
-        self.extendedMode.clicked.connect(self.extendWindow)
+        # Connect each button to it's appropriate method
+        self.basicMode.clicked.connect(self.newBasicWindow)
+        self.extendedMode.clicked.connect(self.newExtendWindow)
 
-    def basicWindow(self):
+    # Create basic methods for the basic and extended modes. These methods 
+    # simply create a new window corresponding to the appropriate mode, whilst
+    # also hiding the initial window and showing the new window.
+    def newBasicWindow(self):
         self.bW = bw.MainWindow()
         self.bW.show()
         self.hide()
 
-    def extendWindow(self):
+    def newExtendWindow(self):
         self.eW = ew.MainWindow()
         self.eW.show()
         self.hide()
 
-        
+# Basic main method which creates a new QApplication and initialises a basic
+# main/initial window.
 def main():
     app = QApplication([])
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
-
-
+# Ensure application is being run from THIS file
 if __name__ == "__main__":
     main()
-
